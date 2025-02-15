cd8166dcee04976ea2ec1b01252cd9144f03f859
ayush <ayushnigam843@gmail.com>
2025-02-15 19:35:37 +0530

    Completed Data Validation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,7 +7,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from src.exception import CustomException
-from src.logger import logging
+from src.logging import logging
 import os
 from src.utils import save_object
 
