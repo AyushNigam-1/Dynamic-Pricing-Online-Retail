e27d3fa10b7615841fe4785667235dc35e146fca
ayush <ayushnigam843@gmail.com>
2025-02-16 20:16:16 +0530

    Fixed Data Validation Issues

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -47,35 +47,35 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-    def split_data_as_train_test(self,dataframe: pd.DataFrame):
-        try:
-            train_set, test_set = train_test_split(
-                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
-            )
-            logging.info("Performed train test split on the dataframe")
+    # def split_data_as_train_test(self,dataframe: pd.DataFrame):
+    #     try:
+    #         train_set, test_set = train_test_split(
+    #             dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+    #         )
+    #         logging.info("Performed train test split on the dataframe")
 
-            logging.info(
-                "Exited split_data_as_train_test method of Data_Ingestion class"
-            )
+    #         logging.info(
+    #             "Exited split_data_as_train_test method of Data_Ingestion class"
+    #         )
             
-            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+    #         dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
             
-            os.makedirs(dir_path, exist_ok=True)
+    #         os.makedirs(dir_path, exist_ok=True)
             
-            logging.info(f"Exporting train and test file path.")
+    #         logging.info(f"Exporting train and test file path.")
             
-            train_set.to_csv(
-                self.data_ingestion_config.training_file_path, index=False, header=True
-            )
+    #         train_set.to_csv(
+    #             self.data_ingestion_config.training_file_path, index=False, header=True
+    #         )
 
-            test_set.to_csv(
-                self.data_ingestion_config.testing_file_path, index=False, header=True
-            )
-            logging.info(f"Exported train and test file path.")
+    #         test_set.to_csv(
+    #             self.data_ingestion_config.testing_file_path, index=False, header=True
+    #         )
+    #         logging.info(f"Exported train and test file path.")
 
             
-        except Exception as e:
-            raise CustomException(e,sys)
+    #     except Exception as e:
+    #         raise CustomException(e,sys)
         
     def initiate_data_ingestion(self):
         try:
--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -2,15 +2,11 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-    trained_file_path:str
-    test_file_path:str
+    feature_store_path:str
 
 
 @dataclass
 class DataValidationArtifact:
     validation_status: bool
-    valid_train_file_path: str
-    valid_test_file_path: str
-    invalid_train_file_path: str
-    invalid_test_file_path: str
+    valid_data_file_path: str
     drift_report_file_path: str
