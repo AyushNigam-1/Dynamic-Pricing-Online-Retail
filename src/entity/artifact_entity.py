c872b20c1824744b34306bdeeea2fcb784f03907
ayush <ayushnigam843@gmail.com>
2025-02-26 17:18:44 +0530

    Fixed Model Training Pipeline

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -18,14 +18,17 @@ class DataTransformationArtifact:
     transformed_train_file_path: str
     transformed_test_file_path: str
 
+
 @dataclass
-class ClassificationMetricArtifact:
-    f1_score: float
-    precision_score: float
-    recall_score: float
+class RegressionMetricArtifact:
+    mae: float  # Mean Absolute Error
+    mse: float  # Mean Squared Error
+    rmse: float  # Root Mean Squared Error
+    r2: float  # R² Score
+
     
 @dataclass
 class ModelTrainerArtifact:
     trained_model_file_path: str
-    train_metric_artifact: ClassificationMetricArtifact
-    test_metric_artifact: ClassificationMetricArtifact
+    train_metric_artifact: RegressionMetricArtifact
+    test_metric_artifact: RegressionMetricArtifact
